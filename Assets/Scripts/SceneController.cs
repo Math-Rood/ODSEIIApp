d8769bfa027bf9d1a033b8e1b3501610574d3acb
Matheus William Pereira Rodrigues <mwpr23121998@gmail.com>
2025-11-13 17:27:14 -0400

    ccccc

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,16 +4,18 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    [SerializeField] private GameObject painelContext;
+    [SerializeField] private GameObject painelScene;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        painelContext.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void CloseContextPanel(){
+        painelContext.SetActive(false);
+        painelScene.SetActive(true);
     }
 
     public void LoadMenuScene(){
